d2dd8c24beff3d14b2a6213f5fb18d810f112c48
AbsoluteDestiny <ian@absolutedestiny.org>
2019-05-18 06:37:34 -0500

    fixes for latest packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,145 +1,171 @@
-var fs = require('fs');
-var NwBuilder = require('nw-builder');
-var gulp = require('gulp');
-var gulpFilter = require('gulp-filter');
-var flatten = require('gulp-flatten');
-var zip = require('gulp-archiver');
-var cp = require('glob-copy');
-var del = require('del');
-var packages = require('./package.json');
+var fs = require("fs");
+var NwBuilder = require("nw-builder");
+var gulp = require("gulp");
+var gulpFilter = require("gulp-filter");
+var flatten = require("gulp-flatten");
+var zip = require("gulp-archiver");
+var cp = require("glob-copy");
+var del = require("del");
+var packages = require("./package.json");
 var version = packages.version;
 console.log(version);
 
 var binMap = {
-    'linux32' : 'build/LlamaEnc3/linux32',
-    'linux64' : 'build/LlamaEnc3/linux64',
-    'osx64' : 'build/LlamaEnc3/osx64/LlamaEnc3.app/Contents/Resources/app.nw',
-    'win32' : 'build/LlamaEnc3/win32',
-    'win64' : 'build/LlamaEnc3/win64'
+  linux32: "build/LlamaEnc3/linux32",
+  linux64: "build/LlamaEnc3/linux64",
+  osx64: "build/LlamaEnc3/osx64/LlamaEnc3.app/Contents/Resources/app.nw",
+  win32: "build/LlamaEnc3/win32",
+  win64: "build/LlamaEnc3/win64"
 };
 
-gulp.task('clean', function(cb) {
-  del(['build', 'dist'], cb);
+gulp.task("clean", function() {
+  return del(["build", "dist"]);
 });
 
-gulp.task('cleanbuild', function(cb) {
-  del(['build'], cb);
+gulp.task("cleanbuild", function() {
+  return del(["build"]);
 });
 
-gulp.task('cleandist', function(cb) {
-  del(['dist'], cb);
+gulp.task("cleandist", function() {
+  return del(["dist"]);
 });
 
-gulp.task('nw', ['clean'], function () {
-    var files = [
-            './**/**',
-            '!./ff*.exe',
-            '!./*.log',
-            '!./bin/**',
-            '!./dist/**',
-            '!./build/**',
-            '!./node_modules/**',
-            '!./cache/**',
-            ];
-    // Don't copy over the dev-dependencies!
-    for (var key in packages.dependencies) {
-        files.push("./node_modules/" + key + "/*.js");
-        files.push("./node_modules/" + key + "/*.json");
-        files.push("./node_modules/" + key + "/**/*.js");
-        files.push("./node_modules/" + key + "/**/*.json");
+gulp.task("nw", ["clean"], function() {
+  var files = [
+    "./**/**",
+    "!./ff*.exe",
+    "!./*.log",
+    "!./bin/**",
+    "!./dist/**",
+    "!./build/**",
+    "!./node_modules/**",
+    "!./cache/**"
+  ];
+  // Don't copy over the dev-dependencies!
+  for (var key in packages.dependencies) {
+    files.push("./node_modules/" + key + "/*.js");
+    files.push("./node_modules/" + key + "/*.json");
+    files.push("./node_modules/" + key + "/**/*.js");
+    files.push("./node_modules/" + key + "/**/*.json");
+  }
+  files = files.concat([
+    "!./node_modules/**/+(test|tests|example|examples)/**"
+  ]);
+  var nw = new NwBuilder({
+    version: "0.38.3",
+    files: files,
+    platforms: ["win32", "win64", "linux64", "linux32"],
+    zip: false,
+    macIcns: "./llama.icns",
+    winIco: "./llama.ico"
+  });
+
+  nw.on("log", console.log);
+
+  // Build returns a promise
+  return nw.build().catch(function(error) {
+    console.log(error);
+    throw error;
+  });
+});
+
+gulp.task("build", ["nw"], function() {
+  var linux32 = gulpFilter("**/linux32/**");
+  var linux64 = gulpFilter("**/linux64/**");
+  var win32 = gulpFilter("**/win32/**");
+  var win64 = gulpFilter("**/win64/**");
+  var osx64 = gulpFilter("**/osx64/**");
+  return gulp
+    .src(["bin/**/**"])
+    .pipe(win64)
+    .pipe(flatten())
+    .pipe(gulp.dest(binMap["win64"]))
+    .pipe(linux32)
+    .pipe(flatten())
+    .pipe(gulp.dest(binMap["linux32"]))
+    .pipe(linux32.restore)
+    .pipe(linux64)
+    .pipe(flatten())
+    .pipe(gulp.dest(binMap["linux64"]))
+    .pipe(linux64.restore)
+    .pipe(win32)
+    .pipe(flatten())
+    .pipe(gulp.dest(binMap["win32"]))
+    .pipe(win32.restore)
+    .pipe(osx64)
+    .pipe(flatten())
+    .pipe(gulp.dest(binMap["osx64"]))
+    .pipe(osx64.restore);
+});
+
+gulp.task("zip", ["cleandist"], function(cb) {
+  var fs = require("fs");
+  var path = require("path");
+  var archiver = require("archiver");
+  var async = require("async");
+  fs.mkdirSync(__dirname + "/dist/");
+
+  async.eachSeries(
+    Object.keys(binMap),
+    function(platform, callback) {
+      var ext = "zip";
+      var type = "zip";
+      var options = {};
+      var mode = {};
+
+      if (
+        platform === "linux64" ||
+        platform === "linux32" ||
+        platform === "osx64"
+      ) {
+        ext = "tar.gz";
+        type = "tar";
+        options = {
+          gzip: true,
+          gzipOptions: {
+            level: 1
+          }
+        };
+        mode = { mode: 0755 };
+      }
+      var output = fs.createWriteStream(
+        path.join(
+          "dist",
+          "LlamaEnc3-" +
+            version +
+            "-" +
+            platform.replace("32", "-32").replace("64", "-64") +
+            "bit." +
+            ext
+        )
+      );
+      var archive = archiver(type, options);
+
+      output.on("close", function() {
+        console.log(archive.pointer() + " total bytes");
+        console.log(
+          "archiver has been finalized and the output file descriptor has closed."
+        );
+        callback();
+      });
+
+      archive.on("error", function(err) {
+        throw err;
+      });
+
+      archive.pipe(output);
+      archive.directory(
+        path.join("./build/LlamaEnc3/", platform),
+        "LlamaEnc3",
+        mode
+      );
+      archive.finalize();
+    },
+    function(err) {
+      if (err) console.error(err.message);
+      cb();
     }
-    files = files.concat(['!./node_modules/**/+(test|tests|example|examples)/**']);
-    var nw = new NwBuilder({
-        version: '0.12.3',
-        files: files,
-        platforms: ['osx64', 'win32', 'win64', 'linux64', 'linux32'],
-        zip: false,
-        macIcns: './llama.icns',
-        winIco: './llama.ico'
-    });
-
-    nw.on('log',  console.log);
-
-    // Build returns a promise
-    return nw.build().catch(function (error) {
-        console.log(error);
-        throw error;
-    });
+  );
 });
 
-gulp.task("build", ['nw'],function() {
-    var linux32 = gulpFilter('**/linux32/**');
-    var linux64 = gulpFilter('**/linux64/**');
-    var win32 = gulpFilter('**/win32/**');
-    var win64 = gulpFilter('**/win64/**');
-    var osx64 = gulpFilter('**/osx64/**');
-    return gulp.src(["bin/**/**"])
-        .pipe(linux32)
-        .pipe(flatten())
-        .pipe(gulp.dest(binMap['linux32']))
-        .pipe(linux32.restore())
-        .pipe(linux64)
-        .pipe(flatten())
-        .pipe(gulp.dest(binMap['linux64']))
-        .pipe(linux64.restore())
-        .pipe(win32)
-        .pipe(flatten())
-        .pipe(gulp.dest(binMap['win32']))
-        .pipe(win32.restore())
-        .pipe(osx64)
-        .pipe(flatten())
-        .pipe(gulp.dest(binMap['osx64']))
-        .pipe(osx64.restore())
-        .pipe(win64)
-        .pipe(flatten())
-        .pipe(gulp.dest(binMap['win64']));
-});
-
-gulp.task('zip', ['cleandist'], function(cb) {
-    var fs = require('fs');
-    var path = require('path');
-    var archiver = require('archiver');
-    var async = require('async');
-    fs.mkdirSync(__dirname + '/dist/');
-
-    async.eachSeries(Object.keys(binMap), function(platform, callback) {
-        var ext = 'zip';
-        var type = 'zip';
-        var options = {};
-        var mode = {};
-
-        if (platform === 'linux64' || platform === 'linux32' || platform === 'osx64') {
-            ext = 'tar.gz';
-            type = 'tar';
-            options = {
-              gzip: true,
-              gzipOptions: {
-                level: 1
-              }
-            };
-            mode = {mode: 0755};
-        }
-        var output = fs.createWriteStream(path.join('dist', 'LlamaEnc3-' + version + '-' + platform.replace('32', '-32').replace('64', '-64') + 'bit.' + ext));
-        var archive = archiver(type, options);
-
-        output.on('close', function() {
-          console.log(archive.pointer() + ' total bytes');
-          console.log('archiver has been finalized and the output file descriptor has closed.');
-          callback();
-        });
-
-        archive.on('error', function(err) {
-          throw err;
-        });
-
-        archive.pipe(output);
-        archive.directory(path.join('./build/LlamaEnc3/', platform), 'LlamaEnc3', mode);
-        archive.finalize();
-    }, function (err) {
-        if (err) console.error(err.message);
-        cb();
-    });
-
-});
-
-gulp.task('default', ['build']);
+gulp.task("default", ["build"]);
--- js/logging.js
+++ js/logging.js
@@ -1,8 +1,8 @@
 function Filter() {
   var self = this;
-  _log = (function (methods, undefined) {
+  _log = (function(methods, undefined) {
     var Log = Error; // does this do anything?  proper inheritance...?
-    Log.prototype.write = function (args, method) {
+    Log.prototype.write = function(args, method) {
       /// <summary>
       /// Paulirish-like console.log wrapper.  Includes stack trace via @fredrik SO suggestion (see remarks for sources).
       /// </summary>
@@ -16,16 +16,16 @@ function Filter() {
 
       // via @fredrik SO trace suggestion; wrapping in special construct so it stands out
       var suffix = {
-          "@": (this.lineNumber ? this.fileName + ':' + this.lineNumber + ":1" // add arbitrary column value for chrome linking
-                  : extractLineNumberFromStack(this.stack)
-          )
+        "@": this.lineNumber
+          ? this.fileName + ":" + this.lineNumber + ":1" // add arbitrary column value for chrome linking
+          : extractLineNumberFromStack(this.stack)
       };
       if (method == "error" && global.LM) {
-        LM.errorMessage(suffix['@'] + '\n\n' + args.join("\n\n"));
+        LM.errorMessage(suffix["@"] + "\n\n" + args.join("\n\n"));
       }
       if (method == "warn" && global.LM) {
         // console.dir(args);
-        LM.warnMessage(suffix['@'] + '\n\n' + args.join("\n\n"));
+        LM.warnMessage(suffix["@"] + "\n\n" + args.join("\n\n"));
       }
 
       args = args.concat([suffix]);
@@ -38,43 +38,47 @@ function Filter() {
         } // nicer display in some browsers
       }
     };
-    var extractLineNumberFromStack = function (stack) {
+    var extractLineNumberFromStack = function(stack) {
       /// <summary>
       /// Get the line/filename detail from a Webkit stack trace.  See http:/stackoverflow.com/a/3806596/1037948
       /// </summary>
       /// <param name="stack" type="String">the stack string</param>
-    
-      // correct line number according to how Log().write implemented
-      // console.log(stack.split('\n')[3]);
-      var line = stack.split('\n')[3];
-      // fix for various display text
-      line = (line.indexOf(' (') >= 0 ? line.split(' (')[1].substring(0, line.length - 1) : line.split('at ')[1]);
-      return line;
-      };
-    
-      // method builder
-      var logMethod = function(method) {
-          return function (params) {
-              /// <summary>
-              /// Paulirish-like console.log wrapper
-              /// </summary>
-              /// <param name="params" type="[...]">list your logging parameters<param>
-              // only if explicitly true somewhere
-              // if (typeof DEBUGMODE === typeof undefined || !DEBUGMODE) return;
+      // if (!stack) {
+      //   return;
+      // }
+      // // correct line number according to how Log().write implemented
+      // console.log(stack.split("\n"));
+      // var line = stack.split("\n")[3];
+      // // fix for various display text
+      // line =
+      //   line.indexOf(" (") >= 0
+      //     ? line.split(" (")[1].substring(0, line.length - 1)
+      //     : line.split("at ")[1];
+      return "";
+    };
 
+    // method builder
+    var logMethod = function(method) {
+      return function(params) {
+        /// <summary>
+        /// Paulirish-like console.log wrapper
+        /// </summary>
+        /// <param name="params" type="[...]">list your logging parameters<param>
+        // only if explicitly true somewhere
+        // if (typeof DEBUGMODE === typeof undefined || !DEBUGMODE) return;
 
-              // call handler extension which provides stack trace
-              Log().write(Array.prototype.slice.call(arguments, 0), method); // turn into proper array & declare method to use
-          };//--  fn  logMethod
-      };
-      var result = logMethod('log'); // base for backwards compatibility, simplicty
-      // add some extra juice
-      for(var i in methods) result[methods[i]] = logMethod(methods[i]);
+        // call handler extension which provides stack trace
+        Log().write(Array.prototype.slice.call(arguments, 0), method); // turn into proper array & declare method to use
+      }; //--  fn  logMethod
+    };
+    var result = logMethod("log"); // base for backwards compatibility, simplicty
+    // add some extra juice
+    for (var i in methods) result[methods[i]] = logMethod(methods[i]);
 
-      return result; // expose
-    })(['log', 'error', 'debug', 'info', 'warn']);//--- _log
+    return result; // expose
+  })(["log", "error", "debug", "info", "warn"]); //--- _log
 
-    self.logger = _log;
+  self.logger = _log;
 }
 
-module.exports = new Filter;
+module.exports = new Filter();
